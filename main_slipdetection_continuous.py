6737f33aa09e129e158d4bfb462ce43716d11fa1
Siyuan <sydong@mit.edu>
2018-10-22 16:31:23 -0400

    add continuous detection

--- main_slipdetection_continuous.py
+++ main_slipdetection_continuous.py
@@ -160,6 +160,106 @@ class slip_detection_reaction:
 
         return x2,y2,u,v
 
+    def flow_calculate_v4(self,keypoints2):
+        x2, y2, u, v, x1_paired, y1_paired, x2_paired, y2_paired = [], [], [], [], [], [], [], []
+
+        for i in range(len(keypoints2)): 
+            x2.append(keypoints2[i].pt[0]/self.scale)
+            y2.append(keypoints2[i].pt[1]/self.scale)
+
+        x2 = np.array(x2) 
+        y2 = np.array(y2)
+        x_initial = list(self.x_iniref)
+        y_initial = list(self.y_iniref)
+        u_ref = list(self.u_addon)
+        v_ref = list(self.v_addon)
+
+        for i in range(x2.shape[0]):
+            
+            distance = list(((np.array(x_initial) - x2[i])**2 + (np.array(y_initial) - y2[i])**2))
+            min_index = distance.index(min(distance))
+            u_temp = x2[i] - x_initial[min_index] 
+            v_temp = y2[i] - y_initial[min_index] 
+            shift_length = np.sqrt(u_temp**2+v_temp**2)
+            # print 'length',shift_length
+
+            if shift_length < 11:
+                # print xy2.shape,min_index,len(distance)
+                x1_paired.append(x_initial[min_index]-u_ref[min_index])
+                y1_paired.append(y_initial[min_index]-v_ref[min_index])
+                x2_paired.append(x2[i])
+                y2_paired.append(y2[i])
+                u.append(u_temp + u_ref[min_index])
+                v.append(v_temp + v_ref[min_index])
+
+                del x_initial[min_index], y_initial[min_index], u_ref[min_index], v_ref[min_index]
+
+                if shift_length > 9: 
+                    self.refresh = True 
+
+        return x1_paired,y1_paired,x2_paired,y2_paired,u,v
+
+
+    def flow_calculate_v5(self,keypoints2):
+        x2, y2, u, v, x1_paired, y1_paired, x2_paired, y2_paired  = [], [], [], [], [], [], [], []
+        x1_return, y1_return, x2_return, y2_return, u_return, v_return = [],[],[],[],[],[]
+
+        for i in range(len(keypoints2)): 
+            x2.append(keypoints2[i].pt[0]/self.scale)
+            y2.append(keypoints2[i].pt[1]/self.scale)
+
+        x2 = np.array(x2) 
+        y2 = np.array(y2)
+        x_initial = list(self.x_iniref)
+        y_initial = list(self.y_iniref)
+        u_ref = list(self.u_addon)
+        v_ref = list(self.v_addon)
+        
+
+        for i in range(x2.shape[0]):
+            distance = list(((np.array(x_initial) - x2[i])**2 + (np.array(y_initial) - y2[i])**2))
+            min_index = distance.index(min(distance))  
+            u_temp = x2[i] - x_initial[min_index] 
+            v_temp = y2[i] - y_initial[min_index] 
+            shift_length = np.sqrt(u_temp**2+v_temp**2)
+            # print 'length',shift_length
+
+            # print xy2.shape,min_index,len(distance)
+            if shift_length < 15:
+                x1_paired.append(x_initial[min_index]-u_ref[min_index])
+                y1_paired.append(y_initial[min_index]-v_ref[min_index])
+                x2_paired.append(x2[i])
+                y2_paired.append(y2[i])
+                u.append(u_temp + u_ref[min_index])
+                v.append(v_temp + v_ref[min_index])
+                # sign = self.ROI[y2[i].astype(np.uint16),x2[i].astype(np.uint16)]
+                # x1_return.append((x_initial[min_index]-u_ref[min_index])*sign)
+                # y1_return.append((y_initial[min_index]-v_ref[min_index])*sign)
+                # x2_return.append((x2[i])*sign)
+                # y2_return.append((y2[i])*sign)
+                # u_return.append((u_temp + u_ref[min_index])*sign)
+                # v_return.append((v_temp + v_ref[min_index])*sign)
+                del x_initial[min_index], y_initial[min_index], u_ref[min_index], v_ref[min_index]   
+
+            
+        print len(self.x_iniref), len(x2_paired)
+        self.x_iniref = list(x2_paired) 
+        self.y_iniref = list(y2_paired)
+        self.u_addon = list(u)
+        self.v_addon = list(v)
+        self.refresh = False 
+
+        inbound_check = self.ROI[np.array(y2).astype(np.uint16),np.array(x2).astype(np.uint16)]*np.array(range(len(x2)))
+        final_list = list(set(inbound_check)- set([0]))
+        x1_return = np.array(x1_paired)[final_list]
+        y1_return = np.array(y1_paired)[final_list]
+        x2_return = np.array(x2_paired)[final_list]
+        y2_return = np.array(y2_paired)[final_list]
+        u_return = np.array(u)[final_list]
+        v_return = np.array(v)[final_list]
+
+        return x1_return, y1_return, x2_return, y2_return, u_return, v_return
+        # return x1_paired,y1_paired,x2_paired,y2_paired,u,v
 
 
     def contact_detection(self,im):
@@ -183,6 +283,7 @@ class slip_detection_reaction:
         img_e = cv2.erode(img_d, self.kernal1, iterations=1)
         img_ee = cv2.erode(img_e, self.kernal2, iterations=1)
         contact = cv2.dilate(img_ee, self.kernal3, iterations=1).astype(np.uint8)
+        # contact_general = cv2.dilate(contact, self.kernal3, iterations=1).astype(np.uint8)
         # contact[:5,:] = 0
         # contact[-5:,:] = 0
         # contact[:,:5] = 0
@@ -196,8 +297,8 @@ class slip_detection_reaction:
 
     def estimate_uv(self,x2,y2,final_list):
         theta = np.arcsin(self.tran_matrix[1,0])
-        x1_select = np.array(self.x_initial)[final_list]
-        y1_select = np.array(self.y_initial)[final_list]
+        x1_select = np.array(self.x1)[final_list]
+        y1_select = np.array(self.y1)[final_list]
         u_select = self.u_sum[final_list]
         v_select = self.v_sum[final_list]
 
@@ -209,18 +310,19 @@ class slip_detection_reaction:
         u_estimate = u_mean + theta*(y_mean - np.array(y2))
         v_estimate = v_mean + theta*(np.array(x2)-x_mean)
 
-        u_estimate[self.trash_list] = 0
-        v_estimate[self.trash_list] = 0
+        # u_estimate[self.trash_list] = 0
+        # v_estimate[self.trash_list] = 0
         return u_estimate, v_estimate
 
     def dispOpticalFlow(self,im_cal,x,y):
         # mask = np.zeros_like(im_cal)
         mask2 = np.zeros_like(im_cal)
-        for i in range(self.u_diff.shape[0]):
+        amf = 1
+        for i in range(self.u_sum.shape[0]):
              # mask = cv2.line(mask, (int(x[i]-self.acc_u[i]*2),int(y[i]-self.acc_v[i]*2)),(int(x[i]),int(y[i])), [0, 80, 0], 2)
              # mask2 = cv2.line(mask2, (int(x[i]+self.u_diff[i]*3),int(y[i]+self.v_diff[i]*3)),(int(x[i]),int(y[i])), [0, 0, 100], 2)
-             mask2 = cv2.line(mask2, (int(x[i]+self.u_estimate[i]*4),int(y[i]+self.v_estimate[i]*4)),(int(x[i]),int(y[i])), [0, 0, 80], 2)
-             mask2 = cv2.line(mask2, (int(x[i]+self.u_sum[i]*4),int(y[i]+self.v_sum[i]*4)),(int(x[i]),int(y[i])), [0, 80, 80], 2)
+             mask2 = cv2.line(mask2, (int(x[i]+self.u_estimate[i]*amf),int(y[i]+self.v_estimate[i]*amf)),(int(x[i]),int(y[i])), [0, 0, 80], 2)
+             mask2 = cv2.line(mask2, (int(x[i]+self.u_sum[i]*amf),int(y[i]+self.v_sum[i]*amf)),(int(x[i]),int(y[i])), [0, 80, 80], 2)
 
         img = cv2.add(im_cal/1.2,mask2)
         # img = im_cal+50
@@ -241,6 +343,7 @@ class slip_detection_reaction:
             # cv2.putText(img,str(self.vel_diff[i]),(int(x[i]),int(y[i])), font, 0.2,(255,255,255),1,cv2.LINE_AA)
         cv2.imshow("force_flow",img.astype(np.uint8))
         cv2.waitKey(1)
+        # raw_input("Press Enter to continue...")
         
     def call_back(self,data):
         t = time.time()
@@ -252,12 +355,7 @@ class slip_detection_reaction:
         # cv2.waitKey(0)
 
         if not self.con_flag:
-            # get the first image and do calibration 
-            # self.contactmask = np.ones((self.img_blur.shape[0:2]),dtype=np.uint8)
-            # self.ROI = np.ones((self.img_blur.shape[0:2]),dtype=np.uint8)
-            # self.contactmask_left = np.ones((self.img_blur.shape[0:2]),dtype=np.uint8)
-            # self.contactmask_right = np.ones((self.img_blur.shape[0:2]),dtype=np.uint8)
-            # self.edge_region = np.ones((self.img_blur.shape[0:2]),dtype=np.uint8)
+
             if self.initial_flag:
                 self.refimage_cali = cv2.warpPerspective(raw_imag, self.M, (self.rows, self.cols))
                 self.ref_finalimage = self.refimage_cali[10:,95:-110,:].astype(np.float32)
@@ -280,22 +378,26 @@ class slip_detection_reaction:
                 if self.index > 10:
                     self.ROI, self.ROI_big =  self.ROI_calculate()
 
-                    self.x_minb = int(np.amin(self.xv*self.ROI_big+(1-self.ROI_big)*255))
-                    self.x_maxb = int(np.amax(self.xv*self.ROI_big))
-                    self.y_minb = int(np.amin(self.yv*self.ROI_big+(1-self.ROI_big)*255))
-                    self.y_maxb = int(np.amax(self.yv*self.ROI_big))   
+                    # self.x_minb = int(np.amin(self.xv*self.ROI_big+(1-self.ROI_big)*255))
+                    # self.x_maxb = int(np.amax(self.xv*self.ROI_big))
+                    # self.y_minb = int(np.amin(self.yv*self.ROI_big+(1-self.ROI_big)*255))
+                    # self.y_maxb = int(np.amax(self.yv*self.ROI_big))   
 
                     final_image = self.creat_mask(im_cal)
-                    keypoints = self.find_dots(final_image*self.ROI)
+                    keypoints = self.find_dots(final_image)
                     # im_with_keypoints = cv2.drawKeypoints(final_image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
                     self.u_sum = np.zeros(len(keypoints))
                     self.v_sum = np.zeros(len(keypoints))
+                    self.u_addon = list(self.u_sum)
+                    self.v_addon = list(self.v_sum)
                     for i in range(len(keypoints)):
                         self.x1_last.append(keypoints[i].pt[0]/self.scale)
                         self.y1_last.append(keypoints[i].pt[1]/self.scale)
                     self.x_initial = list(self.x1_last)
                     self.y_initial = list(self.y1_last)
+                    self.x_iniref = list(self.x1_last)
+                    self.y_iniref = list(self.y1_last)
                     self.con_flag = True
                     self.index = 0
                     print "finish pre-calculation"
@@ -317,34 +419,25 @@ class slip_detection_reaction:
             # print tran_matrix
 
             final_image = self.creat_mask(im_cal)
-            keypoints = self.find_dots(final_image*self.ROI)
-            im_with_keypoints = cv2.drawKeypoints(final_image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            if self.refresh:
+                keypoints = self.find_dots(final_image)
+                self.x1,self.y1,x2,y2,u,v = self.flow_calculate_v5(keypoints)
+            else:
+                keypoints = self.find_dots(final_image*self.ROI)
+                self.x1,self.y1,x2,y2,u,v = self.flow_calculate_v4(keypoints)
+
+
+            # im_with_keypoints = cv2.drawKeypoints(final_image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
             # cv2.imshow("marker",im_with_keypoints.astype(np.uint8))
             # cv2.waitKey(1)
 
-            x2,y2,u,v = self.flow_calculate_v3(keypoints)
-
-            self.trash_list = sorted(set(self.trash_list))
-            self.u_sum += np.array(u)
-            self.v_sum += np.array(v)
-            self.u_sum[self.trash_list] = 0 
-            self.v_sum[self.trash_list] = 0 
-
-            if self.refresh:
-                self.x_initial = self.x2_raw
-                self.y_initial = self.y2_raw 
-                self.x1_last = self.x2_raw
-                self.y1_last = self.y2_raw
-                x2 = self.x2_raw
-                y2 = self.y2_raw
-                self.u_sum = np.zeros(len(self.x2_raw))
-                self.v_sum = np.zeros(len(self.x2_raw))
-                self.trash_list = []
-                self.refresh = False 
+            # self.trash_list = sorted(set(self.trash_list))
+            self.u_sum = np.array(u)
+            self.v_sum = np.array(v)
 
             # good_list = list(set(range(len(x2)))-set(self.trash_list)) 
             inbound_check = self.p_region[np.array(y2).astype(np.uint16),np.array(x2).astype(np.uint16)]*np.array(range(len(x2)))
-            final_list = list(set(inbound_check)- set([0]) - set(self.trash_list))# - set(range(len(u),len(x2))))
+            final_list = list(set(inbound_check)- set([0]))# - set(range(len(u),len(x2))))
             # print "number of points inside", len(set(inbound_check)) #, np.sum(p_region[np.array(y2).astype(np.uint16),np.array(x2).astype(np.uint16)]), len(x2)
             if len(final_list) < 4:
                 self.kernal_size -= 10
@@ -370,13 +463,16 @@ class slip_detection_reaction:
             # print "number of reference points",len(final_list),len(x2),len(self.trash_list)
             x2_center = np.expand_dims(np.array(x2)[final_list],axis = 1)
             y2_center = np.expand_dims(np.array(y2)[final_list],axis = 1)
-            x1_center = np.expand_dims(np.array(self.x_initial)[final_list],axis = 1)
-            y1_center = np.expand_dims(np.array(self.y_initial)[final_list],axis = 1)
+            x1_center = np.expand_dims(np.array(self.x1)[final_list],axis = 1)
+            y1_center = np.expand_dims(np.array(self.y1)[final_list],axis = 1)
             p2_center = np.expand_dims(np.concatenate((x2_center,y2_center),axis = 1),axis = 0)
             p1_center = np.expand_dims(np.concatenate((x1_center,y1_center),axis = 1),axis = 0)
-
-            if len(final_list) >2:
+            print len(final_list)
+            if len(final_list)>1:
                 self.tran_matrix = cv2.estimateRigidTransform(p1_center,p2_center,False)
+            
+
+            # print self.tran_matrix
 
 
             if self.tran_matrix is not None:
@@ -390,18 +486,20 @@ class slip_detection_reaction:
                 # raw_input("Press Enter to continue...")
                 if self.showimage:
                     self.dispOpticalFlow(im_cal,x2,y2)
+
             else: 
                 self.ROI, self.ROI_big =  self.ROI_calculate()
-                self.x_initial = self.x2_raw
-                self.y_initial = self.y2_raw 
-                self.x1_last = self.x2_raw
-                self.y1_last = self.y2_raw
-                x2 = self.x2_raw
-                y2 = self.y2_raw
-                self.u_sum = np.zeros(len(self.x2_raw))
-                self.v_sum = np.zeros(len(self.x2_raw))
-                self.trash_list = []
-                self.refresh = True
+                # # self.x_initial = self.x2_raw
+                # # self.y_initial = self.y2_raw 
+                # self.x1_last = self.x2_raw
+                # self.y1_last = self.y2_raw
+                # x2 = self.x2_raw
+                # y2 = self.y2_raw
+                # self.u_sum = np.zeros(len(self.x2_raw))
+                # self.v_sum = np.zeros(len(self.x2_raw))
+                # self.trash_list = []
+                # self.refresh = True
+                pass
 
             
             if self.tran_matrix is None and len(final_list) >3:
@@ -409,31 +507,31 @@ class slip_detection_reaction:
 
 
             self.pub.publish(self.slip_indicator)
-
+            
             if self.slip_indicator: 
                 print("slip!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") 
                 self.slip_indicator = False
                 self.ROI, self.ROI_big =  self.ROI_calculate()
-                self.x_initial = self.x2_raw
-                self.y_initial = self.y2_raw 
-                self.x1_last = self.x2_raw
-                self.y1_last = self.y2_raw
-                x2 = self.x2_raw
-                y2 = self.y2_raw
-                self.u_sum = np.zeros(len(self.x2_raw))
-                self.v_sum = np.zeros(len(self.x2_raw))
-                self.trash_list = []
-                self.refresh = True
+                # # self.x_initial = self.x2_raw
+                # # self.y_initial = self.y2_raw 
+                # self.x1_last = self.x2_raw
+                # self.y1_last = self.y2_raw
+                # x2 = self.x2_raw
+                # y2 = self.y2_raw
+                # self.u_sum = np.zeros(len(self.x2_raw))
+                # self.v_sum = np.zeros(len(self.x2_raw))
+                # self.trash_list = []
+                # self.refresh = True
 
             
 
 
 
 
-            self.x1_last = x2
-            self.y1_last = y2
+            # self.x1_last = x2
+            # self.y1_last = y2
             # self.trash_list_last = self.trash_list
-        # print 1/(time.time()-t)
+        print 1/(time.time()-t)
 
 # def close_gripper_f(grasp_speed=50, grasp_force=15):
 #     graspinGripper(grasp_speed=grasp_speed, grasp_force=grasp_force)
